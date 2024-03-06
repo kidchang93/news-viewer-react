463c47abba3626b3049ae5974bb2b060dca1e2fa
kidchang93 <lck930721@gmail.com>
2024-03-06 17:35:57 +0900

    json 설정, 불러오기 버튼 활성화

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from '../node_modules/axios/index';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = () => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => {
+        setData(response);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={onClick}>불러오기</button>
+      <div>{data && <textarea value={JSON.stringify(data, null, 2)} />}</div>
     </div>
   );
-}
+};
 
 export default App;
