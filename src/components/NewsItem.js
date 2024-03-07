a748cd6e7c237b48be194a3dc0e4cd8dcf0dc21a
kidchang93 <lck930721@gmail.com>
2024-03-07 13:49:30 +0900

    newsItem 컴포넌트 수정

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-const NewsItem = () => {
+const NewsItem = ({ article }) => {
+  const { title, description, url, urlToImage, author } = article;
   return (
     <div className="col-md-6">
       <div className="card mb-4 box-shadow">
@@ -9,8 +10,24 @@ const NewsItem = () => {
           src="https://placehold.co/160X80"
           alt="Card image cap"
         />
+        {urlToImage && (
+          <img className="card-img-top" src={urlToImage} alt="Card image cap" />
+        )}
         <div className="card-body">
-          <h4>title</h4>
+          <h4>{title}</h4>
+          <p className="card-text">{description}</p>
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="btn-group">
+              <a
+                href={url}
+                target="_blank"
+                className="btn btn-sm btn-outline-secondary"
+              >
+                View
+              </a>
+            </div>
+            <small className="text-muted">{author}</small>
+          </div>
         </div>
       </div>
     </div>
