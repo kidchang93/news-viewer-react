c4d80e3d4fb7593fe3a83934f23c541173dfc345
kidchang93 <lck930721@gmail.com>
2024-03-07 15:47:49 +0900

    카테고리 별로 출력 확인

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -2,20 +2,22 @@ import React, { useEffect, useState } from 'react';
 import NewsItem from './NewsItem';
 import axios from '../../node_modules/axios/index';
 
-const NewsList = () => {
+const NewsList = ({ category }) => {
   const [articles, setArticles] = useState(null);
   const [loading, setLoading] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
+      const query = category == 'all' ? '' : '&category=' + category;
       const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=0a533ac5123b410a94df7609ceb74ffb',
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=0a533ac5123b410a94df7609ceb74ffb' +
+          query,
       );
       setArticles(response.data.articles);
       setLoading(false);
     };
     fetchData();
-  }, []);
+  }, [category]);
   if (loading) {
     return <>대기중...</>;
   }
